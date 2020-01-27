60cf8891bb5bb2037bd4014129fad9dac7dffaf4
Jenna Berlinberg <jenna.berlinberg@gmail.com>
2020-01-27 10:57:28 -0800

    Finish table

--- Classwork/Classwork02/evenOddTable.c
+++ Classwork/Classwork02/evenOddTable.c
@@ -2,7 +2,17 @@
 
 int main(int argc, char * argv[]) {
     int i;
+    char* parity;
+    int evens;
     for( i = -10; i < 11; i++ ) {
-        printf("%d \t %d\n" , i, i*i);
+      if ( i % 2 == 0 ) {
+        parity = "Even";
+        evens++;
+      } else {
+        parity = "Odd";
+      }
+      printf( "%d \t %d \t %s\n" , i, i*i, parity );
+
     }
+    printf( "# of evens: %d\n# of odds: %d\n", evens, 21 - evens );
 }
