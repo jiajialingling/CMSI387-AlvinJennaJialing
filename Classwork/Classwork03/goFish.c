9657c9b66dc0c41c046cfc6d26c60ee04cf4f6c3
jenna <jenna.berlinberg@gmail.com>
2020-01-29 11:04:20 -0800

    Try to figure out entry

--- Classwork/Classwork03/goFish.c
+++ Classwork/Classwork03/goFish.c
@@ -2,15 +2,18 @@
 
 int main(int argc, char * argv[]) {
     int numbers [25];
-    while (argv[2] != -9999) {
-        printf("Enter a number: ");
-        gets(numbers);
+    int entry;
+
+    while (entry != -9999) {
+      printf("Enter a number: ");
+      gets(numbers);
     }
+
+
     int sum = 0;
     for (int i = 0; i < sizeof(numbers); i++) {
         sum = sum + i;
     }
     int avg = sum / 25;
-    
-    
+
 }
