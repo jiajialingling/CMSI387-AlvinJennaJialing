e01e458e4f109a5fe8419a993c6fd07c26218007
Jialing Li <Linannan0505@gmail.com>
2020-02-05 11:04:41 -0800

    work in progress

--- Classwork/Classwork04/schedPractice.c
+++ Classwork/Classwork04/schedPractice.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 int main(int argc, char * argv[]) {
+    char letters[3];
+    int duration[3];
+    for (int i = 0; i < 3; i++) {
+        printf("\nPlease enter a single letter: ");
+        scanf("%s", letters);
+        printf("\nPlease enter a integer duration in mS: ");
+        scanf("%d", duration);
+    }
+    printf("KEY -- thread:used:remaining\n\n");
+    for (int i = 0; i < 3; i++) {
+        if (duration[i] <= 40) {
+            printf("%s:%d:done", letters[i], duration[i]);
+        }
+    }
+    if (duration[i])
     
-
 }
