dd93cc53fb19a517e496e687d6d54c9c8caee08b
Jialing Li <Linannan0505@gmail.com>
2020-02-05 23:02:25 -0800

    completed demographics program, need peer checking

--- Homework/Homework01/demographics.c
+++ Homework/Homework01/demographics.c
@@ -5,25 +5,33 @@ int main(int argc, char * argv[]) {
     int age = -1;
     int classYear = -1;
     char gender[10];
-    char major[40];
-    char occupation[50];
+    char state[50];
+    char siblings[4];
     
     printf("What is your name: ");
-//    scanf("%s", &answers);
-    gets(name, sizeof(name), stdin);
+    fgets(name, sizeof(name), stdin);
+    //this is to handle the extra '\n' character at the end of the array
+    for (int i = 0; i <= sizeof(name); i++) {
+        if(name[i] == '\n') {
+            name[i] = '\0';
+            break;
+        }
+    }
+    
     printf("\nWhat is your age: ");
     scanf("%d", &age);
     printf("\nWhat is your class year: ");
     scanf("%d", &classYear);
-//    printf("What is your name: ");
-//    scanf("%s", answers);
-//    printf("What is your name: ");
-//    scanf("%s", answers);
-//    printf("What is your name: ");
-//    scanf("%s", answers);
+    printf("\nWhat is your gender: ");
+    scanf("%s", gender);
+    printf("\nWhich state were you from: ");
+    scanf("%s", state);
+    printf("\nDo you have siblings (Yes/No): ");
+    scanf("%s", siblings);
+    printf("\nThank you!\n");
     
-    printf("====================\n");
+    printf("\n====================\n");
     printf("    Demographics    \n");
     printf("====================\n");
-    printf("Name: %s\nAge: %d\nClass year: %d\n", name, age, classYear);
+    printf("Name: %s\nAge: %d\nClass year: %d\nGender: %s\nState: %s\nSiblings: %s\n\n", name, age, classYear, gender, state, siblings);
 }
